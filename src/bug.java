d16d24a2ae5f6aaa0579c7e9f20e57d51311cbe2
yamna-ali <yamna_fasih.92@hotmail.com>
2020-08-26 10:42:17 -0400

    first commit

---
+++ src/bug.java
@@ -0,0 +1,13 @@
+
+public class bug{
+
+public static void main(String args[]) {
+
+int i = 8;
+int sum=0;
+        for (; i > 0; i -= 2) {
+                sum += i;
+        }
+
+        System.out.println("Sum=" + sum);
+        }}
